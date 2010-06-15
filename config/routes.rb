a701878f600719f0c5bed59b1076d990ffb48cf0
Nico Schottelius <nico@kr.ethz.ch>
2010-06-15 11:48:37 +0200

    route to user view by default
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rdb::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "user#index"
 
   # See how all your routes lay out with "rake routes"
 
