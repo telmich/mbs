6248582bd02ede89af6f111e1a2a26dba393146a
Nico Schottelius <nico@kr.ethz.ch>
2010-06-23 13:25:07 +0200

    add ram, cores, cpu_type validation
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,8 @@
 class Machine < ActiveRecord::Base
    validates :title, :uniqueness => true
+   validates_presence_of :ram
+   validates_presence_of :cores
+   validates_presence_of :cpu_type
 
    has_many :reservations, :dependent => :destroy
 end
