5ede55776115055f5e2d383de592a7a85a18dd4c
Nico Schottelius <nico@vr.ethz.ch>
2010-07-02 16:01:07 +0200

    verify name, not title
    Signed-off-by: Nico Schottelius <nico@vr.ethz.ch>

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,5 @@
 class Machine < ActiveRecord::Base
-   validates :title, :uniqueness => true
+   validates :name, :uniqueness => true
 
    has_many :reservations, :dependent => :destroy
    belongs_to :machine_type
