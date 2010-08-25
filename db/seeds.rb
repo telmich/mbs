31b4d46bdc22ea36135ba1882b728f212335e6c5
Nico Schottelius <nico@kr.ethz.ch>
2010-08-26 01:00:29 +0200

    add db seeds
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create( :name => 'nico' )
+
+mt = []
+mt << { :name => 'IKQ', :ram_gib => 2, :cores => 2, :cpu_type => "AMD Opteron 250 2.4Ghz", :count => 7, :description => "" } 
+mt << { :name => 'IKR', :ram_gib => 4, :cores => 4, :cpu_type => "AMD Opteron 275 2.2Ghz", :count => 30, :description => "" } 
+mt << { :name => 'Incredibles', :ram_gib => 8, :cores => 2, :cpu_type => "AMD Opteron 248 2.2Ghz", :count => 3, :description => "80/74 GB HD" }
+mt << { :name => 'Mozart', :ram_gib => 8, :cores => 4, :cpu_type => "Intel Xeon 3360 2.8Ghz", :count => 16, :description => "" } 
+mt << { :name => 'Shrek', :ram_gib => 6, :cores => 4, :cpu_type => "Opteron 280 (2.4Ghz)/248 (2.2Ghz)", :count => 12, :description => "80 + (300|400) GB" }
+mt << { :name => 'Dryad', :ram_gib => 16, :cores => 8, :cpu_type => "AMD Opteron 2376 2.3GHz", :count => 16, :description => "With two HDs" }
+mt << { :name => 'Bach', :ram_gib => 24, :cores => 8, :cpu_type => "Intel Xeon L5520 2.26Ghz", :count => 30, :description => "Nehalem EP" }
+mt << { :name => 'sgs-r815-', :ram_gib => 128, :cores => 48, :cpu_type => "AMD Opteron 6174 2.2GHz", :count => 1, :description => "Magny Cours" }
+
+
+mt.each do |type|
+   mtn = MachineType.create type
+   puts "Creating " + type[:name] + "\n"
+end 
+
+
