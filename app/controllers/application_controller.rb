2a8e0cf271c202b0cc99e47a21310880fa6315cf
Nico Schottelius <nico@kr.ethz.ch>
2010-10-01 22:35:31 +0200

    switch to http basic, because we need to retrieve the password
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,24 @@
-require 'rubygems'
-require 'net/ldap'
+#require 'rubygems'
+#require 'net/ldap'
 
+# I need basic auth, see
+# http://kosmaczewski.net/2008/07/07/basic-vs-digest/
+#
 class ApplicationController < ActionController::Base
    protect_from_forgery
    layout 'application'
 
    before_filter :authorize
+   #before_filter :digest_authenticate, :except => [:index, :show]  
+   
+   @realm = "Use your nethz credentials"
 
    def authorize
       puts "Authorize was called"
-   end
-
-   #before_filter :digest_authenticate, :except => [:index, :show]  
-   before_filter :digest_authenticate
-
-   def digest_authenticate
-      puts "Begin auth"
-      success = authenticate_or_request_with_http_digest("MyFancyLogin") do |username|
-         puts "User: " + username + "Pass: " + password
+      success = authenticate_or_request_with_http_basic "test" do |username, password|
+         puts "User: " + username
+         puts "Pass: " + password
+         nethz_auth username, password
       end
    end
 
@@ -30,29 +31,22 @@ class ApplicationController < ActionController::Base
 #    end
 #  end
 
+   def nethz_auth(username, password)
+      ldap = Net::LDAP.new
+      ldap.auth "cn=#{username},ou=users,ou=nethz,ou=id,ou=auth,o=ethz,c=ch", password
 
+      ldap.host = "ldaps02.ethz.ch"
+      ldap.port = "636"
+      ldap.encryption :simple_tls
 
-def nethz_auth(username, password)
-   ldap = Net::LDAP.new
-   ldap.auth "cn=#{username},ou=users,ou=nethz,ou=id,ou=auth,o=ethz,c=ch", password
-
-   ldap.host = "ldaps02.ethz.ch"
-   ldap.port = "636"
-   ldap.encryption :simple_tls
-
-   if ldap.bind
-      puts "Auth worked for #{username}"
-      ok=true
-   else
-      puts "Auth failed for #{username}"
-      ok=false
-   end
- 
-  return ok
-end 
-
-if $0 == __FILE__
-   ldap_bind_test ARGV[0] ARGV[1]
-end
-
+      if ldap.bind
+         puts "Auth worked for #{username}"
+         ok=true
+      else
+         puts "Auth failed for #{username}"
+         ok=false
+      end
+    
+     return ok
+   end 
 end
