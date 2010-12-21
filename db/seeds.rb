77016df10db7c78b46c50e616e9a8c9acd320e48
Nico Schottelius <nico@kr.ethz.ch>
2010-12-21 09:06:57 +0100

    don't mark bootable
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ mbookable = MachineStatus.find_by_name "bookable"
 mt = []
 mt << { :name => 'IKQ', :ram_gib => 2, :cores => 2, :cpu_type => "AMD Opteron 250 2.4Ghz", :count => 7, :description => "" } 
 mt << { :name => 'IKR', :ram_gib => 4, :cores => 4, :cpu_type => "AMD Opteron 275 2.2Ghz", :count => 30, :description => "" } 
-mt << { :name => 'Incredibles', :ram_gib => 8, :cores => 2, :cpu_type => "AMD Opteron 248 2.2Ghz", :count => 3, :description => "80/74 GB HD" }
+mt << { :name => 'Incredibles', :ram_gib => 8, :cores => 2, :cpu_type => "AMD Opteron 248 2.2Ghz", :count => 2, :description => "80/74 GB HD" }
 mt << { :name => 'Mozart', :ram_gib => 8, :cores => 4, :cpu_type => "Intel Xeon 3360 2.8Ghz", :count => 16, :description => "" } 
 mt << { :name => 'Shrek', :ram_gib => 6, :cores => 4, :cpu_type => "Opteron 280 (2.4Ghz)/248 (2.2Ghz)", :count => 12, :description => "80 + (300|400) GB" }
 mt << { :name => 'Dryad', :ram_gib => 16, :cores => 8, :cpu_type => "AMD Opteron 2376 2.3GHz", :count => 16, :description => "With two HDs" }
@@ -33,9 +33,10 @@ mt.each do |type|
    puts "Created machine type " + mtn.name + "\n"
 end 
 
-# mark all as bookable
-Machine.all.each do |m|
-   m.machine_status =  MachineStatus.find_by_name "bookable"
-end
+# mark all as bookable - don't, do that in the migration!
+# Machine.all.each do |m|
+#    m.machine_status =  MachineStatus.find_by_name "bookable"
+#    m.save
+# end
 
 
