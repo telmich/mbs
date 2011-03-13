71226974222002ca3c8604ff608fc0207151654a
Nico Schottelius <nico@kr.ethz.ch>
2011-03-13 16:56:15 +0100

    rethink conflict detection
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -14,9 +14,46 @@ class Machine < ActiveRecord::Base
       booked = nil
 
       bookings.each do |booking|
-         if (booking.begin <= dates[:begin] and booking.end > dates[:begin]) or
-             (dates[:end] > booking.begin and dates[:end] <= booking.end)
+         # Old code: to check bookings
+         # if (booking.begin <= dates[:begin] and booking.end > dates[:begin]) or
+         #     (dates[:end] > booking.begin and dates[:end] <= booking.end)
 
+      # Given:
+      #  - begin and end of requested booking (dates[:begin], dates[:end]
+      #  - begin and end of all existing bookings for the machine
+      #  - new begin may be at the same time old expires (i.e. begin_new == end_old)
+      #
+      # Searched:
+      # - any booking conflict
+      #
+      # Idea:
+      #
+      # - Does the new booking begin after our end?
+         if (dates[:begin] >= booking.end)
+            #
+            #  yes: no conflict possible with this booking
+            #
+            next
+         else
+         #
+         # no: the booking can conflict with the current one
+         #
+
+            #
+            # Does the booking end before our begin?
+            #
+            if (dates[:end] <= booking.begin)
+               #
+               #  yes: no conflict possible with this booking
+               #
+               next
+            else
+               #
+               #  no: new one begins before our end and
+               #      new ones ends after our begin
+               #      -----> conflict
+               #
+               
             # Verify that the booking still exists (i.e. not deleted)
             if booking.existing?
                booked = booking
